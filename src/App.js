d28c630274bd0c39093a42ea4b506ea7ebb9d440
Diego Torres <diegotc86@gmail.com>
2020-02-29 17:42:45 -0500

    Add example of using the websocket

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { subscribeToTimer } from "./api";
 
 function App() {
+  const [timestamp, setTimestamp] = useState("no tiner yet");
+
+  useEffect(() => {
+    subscribeToTimer(1000, (err, timestamp) => setTimestamp(timestamp));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +25,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>This is the timer value: {timestamp}</p>
       </header>
     </div>
   );
