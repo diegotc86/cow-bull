4bc1e78748f14ccf62ab0a25f623840b43fe49cd
Diego Torres <diegotc86@gmail.com>
2020-03-08 18:44:38 -0500

    Merge App componente

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Router } from "@reach/router";
 // views
 import Library from "views/library";
@@ -9,19 +9,39 @@ import PreGame from "views/pre-game/pre-game";
 import Lobby from "views/lobby/lobby";
 import Game from "views/game/game";
 import Gameover from "views/gameover/gameover";
+import SocketContext from "./contexts/SocketContext";
+import io from "socket.io-client";
 
 function App() {
+  const ENDPOINT = "localhost:8000";
+  const [socket, setSocket] = useState("");
+
+  useEffect(() => {
+    setSocket(() => io(ENDPOINT));
+  }, []);
+
+  useEffect(() => {
+    if (!socket) return;
+
+    return () => {
+      socket.emit("disconnect");
+      socket.off();
+    };
+  }, [ENDPOINT, socket]);
+
   return (
-    <Router>
-      <Library path="library" />
-      <Home path="/" />
-      <NewGame path="/new-game" />
-      <JoinGame path="/join-game" />
-      <PreGame path="/pre-game" />
-      <Lobby path="/lobby" />
-      <Game path="/game" />
-      <Gameover path="/gameover" />
-    </Router>
+    <SocketContext.Provider value={socket}>
+      <Router>
+        <Library path="library" />
+        <Home path="/" />
+        <NewGame path="/new-game" />
+        <JoinGame path="/join-game" />
+        <PreGame path="/pre-game" />
+        <Lobby path="/lobby" />
+        <Game path="/game" />
+        <Gameover path="/gameover" />
+      </Router>
+    </SocketContext.Provider>
   );
 }
 
