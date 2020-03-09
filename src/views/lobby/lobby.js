472b6f9ebe78700d88c8152f78c2df103eed0c48
paulportillo <yummta@gmail.com>
2020-03-08 21:32:35 -0500

    Add animations

--- src/views/lobby/lobby.js
+++ src/views/lobby/lobby.js
@@ -1,52 +1,22 @@
 import React from "react";
-import Button from "ui/buttons/button";
 import ContainerApp from "components/container-app";
-import {} from "./lobby.styles";
-import styled from "styled-components";
-import { weight } from "variables";
 import cow from "assets/svg/big-cow.svg";
 import bull from "assets/svg/big-bull.svg";
-import { Image } from "./lobby.styles";
-
-const Body = styled.div`
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-`;
-
-const StyledButton = styled(Button)`
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  font-size: 14px;
-  margin-left: 8px;
-  display: inline-block;
-`;
-
-const Code = styled.span`
-  font-weight: ${weight.bold};
-`;
-
-const Center = styled.div``;
-const TextCenter = styled.p`
-  text-align: center;
-`;
+import * as S from "./lobby.styles";
 
 function Lobby({ roomId }) {
   return (
     <ContainerApp>
-      <Body>
-        <Center>
-          <Image src={cow} width="80" />
-          <Image src={bull} width="80" />
-          <TextCenter>Waiting oponent...</TextCenter>
-        </Center>
-        <StyledButton type="secondary">
-          Copy code room: <Code>{roomId}</Code>
-        </StyledButton>
-      </Body>
+      <S.Body>
+        <S.Center>
+          <S.Image src={cow} width="80" />
+          <S.Image src={bull} width="80" />
+          <S.TextCenter>Waiting oponent...</S.TextCenter>
+        </S.Center>
+        <S.StyledButton type="secondary">
+          Copy code room: <S.Code>{roomId}</S.Code>
+        </S.StyledButton>
+      </S.Body>
     </ContainerApp>
   );
 }
--- src/views/lobby/lobby.styles.js
+++ src/views/lobby/lobby.styles.js
@@ -1,9 +1,41 @@
 import styled from "styled-components";
+import Button from "ui/buttons/button";
+import { weight } from "variables";
 
 const Image = styled.img`
   margin-right: 12px;
   margin-left: 12px;
   vertical-align: top;
+  animation: vibrate-1 1s linear infinite both;
+  &:first-child {
+    animation-delay: 0.5s;
+  }
 `;
 
-export { Image };
+const Body = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const StyledButton = styled(Button)`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  font-size: 14px;
+  margin-left: 8px;
+  display: inline-block;
+`;
+
+const Code = styled.span`
+  font-weight: ${weight.bold};
+`;
+
+const Center = styled.div``;
+const TextCenter = styled.p`
+  text-align: center;
+`;
+
+export { Image, Body, StyledButton, Code, Center, TextCenter };
